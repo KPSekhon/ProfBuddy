d61188109e4be4493777fa3fe4a705e125d795b1
newnewnewgen <151399690+newnewnewgen@users.noreply.github.com>
2023-11-19 15:47:42 -0800

    Revert "merge"
    This reverts commit 2d2976ff5fa956c6f96ba2a3e61ee81928c76ff5.

--- ProfBuddyApp/StudentLogin.js
+++ ProfBuddyApp/StudentLogin.js
@@ -1,40 +1,50 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import { Avatar, Card, Title, Paragraph } from 'react-native-paper';
+import { Avatar, Card } from 'react-native-paper';
 
-const LoginScreen = () => {
+const StudentLogin = ({ navigation }) => {
   const [cwl, setCwl] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleNext = () => {
+    // You can perform any necessary actions with the form data here
+    // For now, let's just log the data to the console
+    console.log('CWL:', cwl);
+    console.log('Password:', password);
+
+    // Navigate to the next screen
+    navigation.navigate('StudentDetails'); // Replace 'NextScreen' with the actual name of the next screen
+  };
 
   return (
     <View style={styles.container}>
       <Card style={styles.card}>
         <Card.Content>
-            <Text style={styles.Title}>Student</Text>
-            <Avatar.Image
-                size={150}
-                source={{ uri: 'https://cdna.artstation.com/p/assets/images/images/040/951/926/large/maddie_creates-jj-ver2.jpg?1630351796' }}
-                style={styles.avatar}
-                />
+          <Text style={styles.Title}>Student</Text>
+          <Avatar.Image
+            size={150}
+            source={{ uri: 'https://cdna.artstation.com/p/assets/images/images/040/951/926/large/maddie_creates-jj-ver2.jpg?1630351796' }}
+            style={styles.avatar}
+          />
 
           <Text style={styles.label}>Enter CWL</Text>
-          <TextInput 
-            style={styles.input} 
-            value={cwl} 
-            onChangeText={text => setCwl(text)} 
-            placeholder="CWL" 
+          <TextInput
+            style={styles.input}
+            value={cwl}
+            onChangeText={(text) => setCwl(text)}
+            placeholder="CWL"
           />
 
           <Text style={styles.label}>Enter Password</Text>
-          <TextInput 
-            style={styles.input} 
-            value={password} 
-            onChangeText={text => setPassword(text)} 
-            placeholder="Password" 
-            secureTextEntry 
+          <TextInput
+            style={styles.input}
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            placeholder="Password"
+            secureTextEntry
           />
 
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity style={styles.button} onPress={handleNext}>
             <Text style={styles.buttonText}>Next</Text>
           </TouchableOpacity>
         </Card.Content>
@@ -73,12 +83,12 @@ const styles = StyleSheet.create({
   Title: {
     fontWeight: 'bold',
     marginBottom: 5,
-    color: "blue",
+    color: '#141B61',
     fontSize: 24,
     lineHeight: 30,
   },
   button: {
-    backgroundColor: 'blue',
+    backgroundColor: '#141B61',
     padding: 10,
     alignItems: 'center',
     borderRadius: 5,
@@ -89,4 +99,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default LoginScreen;
+export default StudentLogin;
